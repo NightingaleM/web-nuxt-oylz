101a52052d77c5966c9acd68c90b83f9a95a19ea
oy_runchi <oy_runchi@qq.com>
2019-08-28 16:50:26 +0800

    添加平台判断

--- pages/index.vue
+++ pages/index.vue
@@ -60,7 +60,15 @@ export default {
     if (process.client) {
       let status = getCookie('oylzft')
       if (!status) {
-        this.$router.replace('/welcome')
+        if (
+          navigator.userAgent.match(
+            /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+          )
+        ) {
+          location.href = '/loading.html'
+        } else {
+          this.$router.replace('/welcome')
+        }
       }
     }
   },
